d74e30bec149979c418f74b95b69b6613a79fbba
mikoda <c0b2213944@edu.teu.ac.jp>
2023-11-21 15:45:39 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img = pg.image.load("ex01/fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)] #練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習4
+        screen.blit(kk_imgs[tmr%2], [300, 200]) #練習5
         pg.display.update()
         tmr += 1        
         clock.tick(10)
